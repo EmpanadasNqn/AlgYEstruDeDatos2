aad74b47ba4e9bb8631f8fc09de02a779d256e0d
EmpanadasNqn <santino.ponchiardi@mi.unc.edu.ar>
2024-05-21 13:19:31 -0300

    {ADD}Lab05[ej3], {M}Lab05[ej2(b)]

--- lab05/ej2b/stack.c
+++ lab05/ej2b/stack.c
@@ -5,6 +5,7 @@
 struct _s_stack {
     stack_elem elem;
     struct _s_stack *next;
+    unsigned int size;
 };
 
 /* Constructors */
@@ -14,9 +15,17 @@ stack stack_empty(void) {
 }
 
 stack stack_push(stack s, stack_elem e) {
+    assert(s == NULL || s->size == stack_size(s));
     stack p = malloc(sizeof(struct _s_stack));
     p->elem = e;
     p->next = s;
+
+    if (stack_is_empty(s)) {
+        p->size = 1u;
+    } else {
+        p->size = stack_size(s) + 1u;
+    }
+    assert(p->size == stack_size(p));
     return p;
 }
 
@@ -24,20 +33,22 @@ stack stack_push(stack s, stack_elem e) {
 
 stack stack_pop(stack s) {
     assert(!stack_is_empty(s));
+    assert(s->size == stack_size(s));
     stack s_aux = s;
     s = s->next;
+    if (!stack_is_empty(s)) {
+        s->size = stack_size(s) - 1;
+    }
     free(s_aux);
+    assert(s == NULL || s->size == stack_size(s));
     return s;
 }
 
 unsigned int stack_size(stack s) {
-    unsigned int size = 0u;
-    stack s_aux = s;
-    while (s_aux != NULL) {
-        s_aux = s_aux->next;
-        size++;
+    if (stack_is_empty(s)) {
+        return 0u;
     }
-    return size;
+    return s->size;
 }
 
 stack_elem stack_top(stack s) {
@@ -53,7 +64,7 @@ stack_elem *stack_to_array(stack s) {               //El orden en el que los ord
     stack_elem *arr;
     stack s_aux = s;
     int contador = 0;
-    const unsigned int size = stack_size(s);
+    unsigned int size = stack_size(s);
 
     if (size == 0) {
         arr = NULL;
--- lab05/ej3/stack.c
+++ lab05/ej3/stack.c
@@ -8,3 +8,54 @@ struct _s_stack {
     unsigned int capacity;  // Capacidad actual del arreglo elems
 };
 
+stack stack_empty(void) {
+    return NULL;
+}
+
+stack stack_push(stack s, stack_elem e) {
+    if (!stack_is_empty(s)) {
+        if (s->size >= s->capacity) {
+            s->elems = realloc(s->elems, sizeof(s->capacity) * 2);
+        }
+        s->elems[s->size] = e;
+        s->size = s->size + 1;
+    } else {
+        s->elems = malloc(sizeof(s->capacity)); //COrregir
+        s->elems[s->size] = e;
+        s->size = 0;
+    }
+    return s;    
+}
+
+stack stack_pop(stack s) {
+    assert(!stack_is_empty(s));
+    s->size = s->size - 1;
+    return s;
+}
+
+unsigned int stack_size(stack s) {
+    if (stack_is_empty(s)) {
+        return 0u;
+    }
+    return s->size;
+}
+
+stack_elem stack_top(stack s) {
+    assert(!stack_is_empty(s));
+    return s->elems[s->size];
+}
+
+bool stack_is_empty(stack s) {
+    return (s == NULL);
+}
+
+stack_elem *stack_to_array(stack s) {
+    return s->elems;
+}
+
+stack stack_destroy(stack s) {
+    free(s->elems);
+    free(s);
+    s = NULL;
+    return s;
+}
